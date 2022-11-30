375d2e34424e8ce3b80f24f91cbf7b3efb94e526
Matthew Semmens <matthew.semmens@cdu.edu.au>
2022-12-01 08:16:58 +0930

    Template

---
+++ Template.py
@@ -0,0 +1,32 @@
+def parser():
+    while 1:
+        data = list(input().split(' '))
+        for number in data:
+            if len(number) > 0:
+                yield(number)   
+
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
+        
+def lParser():
+    while 1:
+        data = list(input().split('\n'))
+        for number in data:
+            if len(number) > 0:
+                yield(number)
+                
+line_parser = lParser()
+
+def get_line():
+    global line_parser
+    return next(line_parser)
