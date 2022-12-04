81a3facef9290678a9f2ef2860d5bb60ddebdf1b
Matthew Semmens <git@matavt.net>
2022-12-04 13:11:51 +0930

    Day 3

---
+++ Day 3/Puzzle1.py
@@ -0,0 +1,76 @@
+def main():
+    charVal = {"a":1,
+        "b":2,
+        "c":3,
+        "d":4,
+        "e":5,
+        "f":6,
+        "g":7,
+        "h":8,
+        "i":9,
+        "j":10,
+        "k":11,
+        "l":12,
+        "m":13,
+        "n":14,
+        "o":15,
+        "p":16,
+        "q":17,
+        "r":18,
+        "s":19,
+        "t":20,
+        "u":21,
+        "v":22,
+        "w":23,
+        "x":24,
+        "y":25,
+        "z":26,
+        "A":27,
+        "B":28,
+        "C":29,
+        "D":30,
+        "E":31,
+        "F":32,
+        "G":33,
+        "H":34,
+        "I":35,
+        "J":36,
+        "K":37,
+        "L":38,
+        "M":39,
+        "N":40,
+        "O":41,
+        "P":42,
+        "Q":43,
+        "R":44,
+        "S":45,
+        "T":46,
+        "U":47,
+        "V":48,
+        "W":49,
+        "X":50,
+        "Y":51,
+        "Z":52
+    }
+
+    loop = True
+    items = []
+    notFound = [] 
+    while loop:
+        rucksack = input()
+        if rucksack == "end":
+            loop = False
+        else:
+            length = len(rucksack)
+            firstHalf = rucksack[0:int(length/2)]
+            secondHalf = rucksack[int(length/2):len(rucksack)]
+            insec = set(firstHalf).intersection(set(secondHalf))
+            if len(insec) == 1:
+                items.append(str(insec.pop()))
+    score = 0
+    for item in items:
+        score += charVal[item]
+    print(score)
+
+if __name__ == ("__main__"):
+    main()
---
+++ Day 3/Puzzle2.py
@@ -0,0 +1,75 @@
+def main():
+    charVal = {"a":1,
+        "b":2,
+        "c":3,
+        "d":4,
+        "e":5,
+        "f":6,
+        "g":7,
+        "h":8,
+        "i":9,
+        "j":10,
+        "k":11,
+        "l":12,
+        "m":13,
+        "n":14,
+        "o":15,
+        "p":16,
+        "q":17,
+        "r":18,
+        "s":19,
+        "t":20,
+        "u":21,
+        "v":22,
+        "w":23,
+        "x":24,
+        "y":25,
+        "z":26,
+        "A":27,
+        "B":28,
+        "C":29,
+        "D":30,
+        "E":31,
+        "F":32,
+        "G":33,
+        "H":34,
+        "I":35,
+        "J":36,
+        "K":37,
+        "L":38,
+        "M":39,
+        "N":40,
+        "O":41,
+        "P":42,
+        "Q":43,
+        "R":44,
+        "S":45,
+        "T":46,
+        "U":47,
+        "V":48,
+        "W":49,
+        "X":50,
+        "Y":51,
+        "Z":52
+    }
+
+    loop = True
+    badges = []
+    while loop:
+        elf1 = input()
+        if elf1 == "end":
+            loop = False
+        else:
+            elf2 = input()
+            elf3 = input()
+
+            insec = set(elf1).intersection(set(elf2)).intersection(elf3)
+            if len(insec) == 1:
+                badges.append(str(insec.pop()))
+    score = 0
+    for badge in badges:
+        score += charVal[badge]
+    print(score)
+
+if __name__ == ("__main__"):
+    main()
