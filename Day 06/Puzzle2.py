60d29d9a70125da83e56c50da6d5873ce991f8ac
Matthew Semmens <matthew.semmens@cdu.edu.au>
2022-12-06 16:00:00 +0930

    Day 6

---
+++ Day 06/Puzzle2.py
@@ -0,0 +1,14 @@
+def main():
+    dataSet = input()
+    check = 14
+    i = check
+    while i < len(dataSet):
+        dataList = []
+        for j in range(i-check,i):
+            dataList.append(dataSet[j])
+        if len(dataList) == len(set(dataList)):
+            print(i)
+            i = len(dataSet)
+        i += 1
+if __name__ == ("__main__"):
+    main()
